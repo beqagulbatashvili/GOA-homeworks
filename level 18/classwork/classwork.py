fdc2cbcbe7d7e2c52cf2826cd743be504b2b45de
beqagulbatashvili <beqagulbatashvili1@gmail.com>
2024-12-15 21:21:43 +0400

    goa

---
+++ level 18/classwork/classwork.py
@@ -0,0 +1,13 @@
+range1 = range(10)
+range2 = range(16)
+range3 = range(20)
+
+
+range4 = range(2,11)
+range5 = range(4,7)
+range6 = range(3,16)
+
+
+range7 = range(2,8,2)
+range8 = range(10,41,5)
+range9 = range(3,16,3)

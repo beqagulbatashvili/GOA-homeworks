2682d4b5610cb2e30e0ae4e67d2fc166de1b49e8
beqagulbatashvili <beqagulbatashvili1@gmail.com>
2024-12-18 23:06:32 +0400

    goa

---
+++ level 20/homework/homework1.py
@@ -0,0 +1,26 @@
+for i in range(10,30):
+    print(i)
+
+for i in range(20,41):
+    print(i)
+
+for i in range(10,30):
+    print(i)
+
+for i in range(51,60,2):
+    print(i)
+
+range1 = range(10,20)
+numbers_sum = 0
+
+for i in range1:
+    numbers_sum=numbers_sum+i
+    print(numbers_sum)
+
+
+range2 = range(2,30,2)
+numbers_sum = 0
+
+for i in range2:
+    numbers_sum=numbers_sum+i
+    print(numbers_sum)

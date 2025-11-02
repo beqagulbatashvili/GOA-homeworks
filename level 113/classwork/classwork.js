ca5a62723c657c95db090454efc5b5586c76823e
beqagulbatashvili <beqagulbatashvili1@gmail.com>
2025-11-02 14:39:03 +0400

    goa

---
+++ level 113/classwork/classwork.js
@@ -0,0 +1,3 @@
+const title = <h1>ეს არის სათაური</h1>;
+const text = <p>ეს არის პარაგრაფი JSX-ის გამოყენებით.</p>;
+const button = <button>დამაჭირე!</button>;
---
+++ level 113/classwork/classwork1.js
@@ -0,0 +1,13 @@
+const myFirstWebpage = (
+    <div>
+        <h1>My First Webpage</h1>
+        <p>
+        Hi, my name is Henry and this is my first website. I am interested in:
+        </p>
+        <ul>
+            <li>Web design</li>
+            <li>Coding</li>
+            <li>Digital art</li>
+        </ul>
+    </div>
+);

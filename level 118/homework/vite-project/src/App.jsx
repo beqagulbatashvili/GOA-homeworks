cc1343ee451925c9e995c9a0d4ee308422ad5ca1
beqagulbatashvili <beqagulbatashvili1@gmail.com>
2025-11-20 17:30:20 +0400

    goa

---
+++ level 118/homework/vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+function HeadingWithParagraph() {
+  return (
+    <div>
+      <h1>This is a Heading</h1>
+      <p>This is a paragraph describing something important.</p>
+    </div>
+  );
+}
+
+export default HeadingWithParagraph;
